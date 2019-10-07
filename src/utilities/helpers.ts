68f3a66f395effd9dbf5985a4b85b0db11b4fb2b
jamesvweston <james@approveme.com>
2019-10-07 14:23:23 -0400

    Figuring out typescript...

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,10 +1,10 @@
 import { isArray, isObject, mapKeys, snakeCase, camelCase, mapValues } from 'lodash'
 
-export const keysToSnakeCase = (object: any): any => mapKeys(object, (v, k) => snakeCase(k))
+export const keysToSnakeCase = (object) => mapKeys(object, (v, k) => snakeCase(k))
 
-export const keysToCamelCase = (object: any): any => mapKeys(object, (v, k) => camelCase(k))
+export const keysToCamelCase = (object) => mapKeys(object, (v, k) => camelCase(k))
 
-export const deepKeysToCamelCase = (object: any): any => {
+export const deepKeysToCamelCase = (object) => {
 	if (isArray(object)) return object.map(deepKeysToCamelCase)
 	const obj = mapValues(object, (v, k) => (isObject(v) ? deepKeysToCamelCase(v) : v))
 	return keysToCamelCase(obj)
