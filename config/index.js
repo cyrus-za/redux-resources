c422221c85ed539bb6df8c0b851a9f52d39b0d4f
jamesvweston <james@approveme.com>
2019-10-08 13:30:03 -0400

    Removing google client id from config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 export default {
 	apiBase: process.versions.node ? '' : process.env.REACT_APP_API_BASE_URL,
 	google: {
-		clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
 		scopes: {
 			authentication: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
 			contacts: 'https://www.googleapis.com/auth/contacts.readonly',
